770ac9c51213aeeaeee64631ea09f48f6d79960b
Enzo Brum <darosabrumenzo@gmail.com>
2023-11-24 00:35:42 -0300

    Tudo funcionando; falta só tirar definições do .h

--- fs.cc
+++ fs.cc
@@ -119,11 +119,10 @@ int INE5412_FS::fs_mount()
 		for (int j = 0; j < INODES_PER_BLOCK; ++j) {
 			fs_inode inode = inode_block.inode[j];
 			if (inode.isvalid) {
-				for (int k = 0; k < POINTERS_PER_INODE; ++k) {
-					if (inode.direct[k]) {
+				for (int k = 0; k < POINTERS_PER_INODE; ++k)
+					if (inode.direct[k])
 						free_blocks[inode.direct[k]] = false;
-					}
-				}
+				
 				if (inode.indirect) {
 					free_blocks[inode.indirect] = false;
 					fs_block indirect_block = read_block(inode.indirect);
@@ -151,72 +150,72 @@ int INE5412_FS::fs_umount() {
 	return 1;
 }
 
+bool INE5412_FS::is_usable() {
+	return mounted;
+}
+bool INE5412_FS::is_usable(int inumber) {
+	return mounted && inumber_is_valid(inumber);
+}
+
 int INE5412_FS::fs_create() {
-    if (!mounted) {
-        cout << "Error: File system is not mounted.\n";
+    if (!is_usable())
         return 0; // Return failure
-    }
 
     // Find a free inode
-    int freeInode = find_free_inode();
+    auto result = find_free_inode();
 
-    if (freeInode == 0) {
+    if (!result.has_value()) {
         cout << "Error: No free inodes available.\n";
         return 0; // Return failure
     }
 
-    // Mark the inode as used
-    fs_block inodeBlock = read_block(1 + (freeInode - 1) / INODES_PER_BLOCK);
-    fs_inode *inode = &inodeBlock.inode[(freeInode - 1) % INODES_PER_BLOCK];
-    inode->isvalid = 1; // Mark the inode as valid
-    inode->size = 0;    // New inode with zero length
+	int inumber = result.value().first;
+	fs_block inodeBlock = result.value().second;
+	fs_inode *inode = &inodeBlock.inode[find_inode_offset(inumber)];
+	
+	inode->isvalid = 1; // Mark the inode as valid
+	inode->size = 0;    // New inode with zero length
 
 	for (int i = 0; i < POINTERS_PER_INODE; ++i)
 		inode->direct[i] = 0;
 	inode->indirect = 0;
 
-    // Write the updated inode block back to disk
-    disk->write(1 + (freeInode - 1) / INODES_PER_BLOCK, inodeBlock.data);
+	// Write the updated inode block back to disk
+	disk->write(find_inode_block(inumber), inodeBlock.data);
 
-    // Step 3: Return the inode number (positive)
-    return freeInode;
+	// Step 3: Return the inode number (positive)
+	return inumber;
 }
 
-int INE5412_FS::find_free_inode() {
+optional<pair<int,INE5412_FS::fs_block>> INE5412_FS::find_free_inode() {
     // Iterate through inodes to find the first free one
-    for (int i = 1; i <= superblock.ninodes; ++i) {
-        fs_block inodeBlock = read_block(1 + (i - 1) / INODES_PER_BLOCK);
-        fs_inode *inode = &inodeBlock.inode[(i - 1) % INODES_PER_BLOCK];
+    for (int i = 1; i <= superblock.ninodeblocks; ++i) {
+        fs_block inodeBlock = read_block(i);
 
-        if (!inode->isvalid) {
-            // Mark the inode as used
-            inode->isvalid = 1;
-            disk->write(1 + (i - 1) / INODES_PER_BLOCK, inodeBlock.data);
+		for (int j = 0; j < INODES_PER_BLOCK; ++j) {
+			fs_inode *inode = &inodeBlock.inode[j];
 
-            return i; // Return the inode number
-        }
+			if (!inode->isvalid) {
+				// Mark the inode as used
+				inode->isvalid = 1;
+				return {{(i-1)*INODES_PER_BLOCK + j + 1, inodeBlock}}; // Return the inode number
+			}
+		}
     }
 
-    return 0; // No free inode found
+    return {}; // No free inode found
 }
 
 int INE5412_FS::fs_delete(int inumber)
 {
-	if (!mounted) {
-        cout << "Error: File system is not mounted.\n";
-        return 0;
-	}
-
-	// Check if the given inode number is valid
-	if (inumber <= 0 || inumber > superblock.ninodes) {
-		cout << "Error: Invalid inode number.\n";
+	if (!is_usable(inumber))
 		return 0;
-	}
-
+	
 	// Read the inode block containing the target inode
-	fs_block inodeBlock = read_block(1 + (inumber - 1) / INODES_PER_BLOCK);
-	fs_inode *inode = &inodeBlock.inode[(inumber - 1) % INODES_PER_BLOCK];
-
+	fs_block inodeBlock = read_block( find_inode_block(inumber) );
+	fs_inode *inode = &inodeBlock.inode[find_inode_offset(inumber)];
+	
+	
 	// Check if the inode is valid
 	if (!inode->isvalid) {
 		cout << "Error: Inode is not valid.\n";
@@ -246,27 +245,19 @@ int INE5412_FS::fs_delete(int inumber)
 	inode->isvalid = 0;
 
 	// Write the update inode block back to the disk
-	disk->write(1 + (inumber - 1) / INODES_PER_BLOCK, inodeBlock.data);
+	disk->write(find_inode_block(inumber), inodeBlock.data);
 	return 1;
 
 }
 
 int INE5412_FS::fs_getsize(int inumber)
 {
-	if (!mounted) {
-        cout << "Error: File system is not mounted.\n";
-        return -1; 
-    }
-
-	// Check if the given inode number is valid
-	if (inumber <= 0 || inumber > superblock.ninodes) {
-		cout << "Error: Invalid inode number.\n";
+	if (!is_usable(inumber))
 		return 0;
-	}
 
 	// Read the inode block containing the target inode
-	fs_block inodeBlock = read_block(1 + (inumber - 1) / INODES_PER_BLOCK);
-	fs_inode *inode = &inodeBlock.inode[(inumber - 1) % INODES_PER_BLOCK];
+	fs_block inodeBlock = read_block( find_inode_block(inumber) );
+	fs_inode *inode = &inodeBlock.inode[find_inode_offset(inumber)];
 
 	// Check if the inode is valid
 	if (!inode->isvalid) {
@@ -280,20 +271,12 @@ int INE5412_FS::fs_getsize(int inumber)
 
 int INE5412_FS::fs_read(int inumber, char *data, int length, int offset)
 {
-	if (!mounted) {
-        cout << "Error: File system is not mounted.\n";
-        return 0; 
-    }
-
-	// Check if the given inode number is valid
-	if (inumber <= 0 || inumber > superblock.ninodes) {
-		cout << "Error: Invalid inode number.\n";
+	if (!is_usable(inumber))
 		return 0;
-	}
 
 	// Read the inode block containing the target inode
-	fs_block inodeBlock = read_block(1 + (inumber - 1) / INODES_PER_BLOCK);
-	fs_inode *inode = &inodeBlock.inode[(inumber - 1) % INODES_PER_BLOCK];
+	fs_block inodeBlock = read_block( find_inode_block(inumber) );
+	fs_inode *inode = &inodeBlock.inode[find_inode_offset(inumber)];
 
 	// Check if the inode is valid
 	if (!inode->isvalid) {
@@ -311,51 +294,32 @@ int INE5412_FS::fs_read(int inumber, char *data, int length, int offset)
 	
 	// Read data from the inode starting at the offset
 	int bytesRead = 0;
-
+  int *indirect_pointers = nullptr;
 	while (bytesRead < effectiveLength) {
 		// Calculate the block index and position within the block
-		int blockIndex = (offset + bytesRead) / Disk::DISK_BLOCK_SIZE;
 		int blockOffset = (offset + bytesRead) % Disk::DISK_BLOCK_SIZE;
-
-		// Read the block containing the data
-		fs_block dataBlock;
-
-		if (blockIndex < POINTERS_PER_INODE) {
-			// Direct block
-			disk->read(inode->direct[blockIndex], dataBlock.data);
-		} else {
-			// Indirect block
-			int indirectIndex = blockIndex - POINTERS_PER_INODE;
-			disk->read(inode->indirect, dataBlock.data);
-			disk->read(dataBlock.pointers[indirectIndex], dataBlock.data);
-		}
+    fs_block dataBlock = read_block(inode, offset+bytesRead, &indirect_pointers);
 
 		// Copy data from the block to the provided data pointer
 		int bytesToCopy = min(effectiveLength - bytesRead, Disk::DISK_BLOCK_SIZE - blockOffset);
 		for (int i = 0; i < bytesToCopy; i++) {
 			data[bytesRead++] = dataBlock.data[blockOffset++];
 		}
-	
 	}
+
+  if (indirect_pointers)
+    delete[] indirect_pointers;
 	return bytesRead;
 }
 
 int INE5412_FS::fs_write(int inumber, const char *data, int length, int offset)
 {
-	if (!mounted) {
-        cout << "Error: File system is not mounted.\n";
-        return 0; 
-    }
-
-	// Check if the given inode number is valid
-	if (inumber <= 0 || inumber > superblock.ninodes) {
-		cout << "Error: Invalid inode number.\n";
+	if (!is_usable(inumber))
 		return 0;
-	}
 
 	// Read the inode block containing the target inode
-	fs_block inodeBlock = read_block(1 + (inumber - 1) / INODES_PER_BLOCK);
-	fs_inode *inode = &inodeBlock.inode[(inumber - 1) % INODES_PER_BLOCK];
+	fs_block inodeBlock = read_block( find_inode_block(inumber) );
+	fs_inode *inode = &inodeBlock.inode[find_inode_offset(inumber)];
 
 	// Check if the inode is valid
 	if (!inode->isvalid) {
@@ -374,51 +338,33 @@ int INE5412_FS::fs_write(int inumber, const char *data, int length, int offset)
 	
 	// Write data from the inode starting at the offset
 	int bytesWritten = 0;
-
 	while (bytesWritten < effectiveLength) {
 		// Calculate the block index and position within the block
-		int blockIndex = (offset + bytesWritten) / Disk::DISK_BLOCK_SIZE;
 		int blockOffset = (offset + bytesWritten) % Disk::DISK_BLOCK_SIZE;
+		int blockIndex = (offset + bytesWritten) / Disk::DISK_BLOCK_SIZE;
 
-		// Check if a new block needs to be allocated
+    
+    int newBlock = find_free_iblock();
+    if (!newBlock) {
+      return bytesWritten;
+    }
+    
+    free_blocks[newBlock] = false;
+    
+		// Check if newBlock is direct
 		if (blockIndex < POINTERS_PER_INODE && inode->direct[blockIndex] == 0) {
-			// Allocate a new direct block
-			int newBlock = find_free_iblock();
-			if (newBlock == 0) {
-				// Disk is full, return total bytes written so far
-				return bytesWritten;
-			}
-			inode->direct[blockIndex] = newBlock;
-			free_blocks[newBlock] = false;
-		} else if (blockIndex >= POINTERS_PER_INODE && inode->indirect == 0) {
-			// Allocate a new indirect block
-			int newBlock = find_free_iblock();
-			if (newBlock == 0) {
-				// Disk is full, return total bytes written so far
-				return bytesWritten;
-			}
-			inode->indirect = newBlock;
-			free_blocks[newBlock] = false;
-
-			// Initialize the indirect block
-			fs_block indirectBlock;
+      inode->direct[blockIndex] = newBlock;
+    } else {
+      fs_block indirectBlock;
 			for (int i = 0; i < POINTERS_PER_BLOCK; ++i) {
 				indirectBlock.pointers[i] = 0;
 			}
 			disk->write(inode->indirect, indirectBlock.data);
-		}
+    }
 
-		// Read the block containing the data
+
+		// create the block containing the data
 		fs_block dataBlock;
-
-		if (blockIndex < POINTERS_PER_INODE) {
-			// Direct block
-			disk->read(inode->direct[blockIndex], dataBlock.data);
-		} else {
-			// Indirect block
-			disk->read(inode->indirect, dataBlock.data);
-			disk->read(dataBlock.pointers[blockIndex - POINTERS_PER_INODE], dataBlock.data);
-		}
 
 		// Copy data from the provided data pointer to the block
 		int bytesToCopy = min(effectiveLength - bytesWritten, Disk::DISK_BLOCK_SIZE - blockOffset);
@@ -437,13 +383,14 @@ int INE5412_FS::fs_write(int inumber, const char *data, int length, int offset)
 		}
 	}
 
+
 	// Update the inode size if needed
 	if (offset + bytesWritten > inode->size) {
 		inode->size = offset + bytesWritten;
 	}
 
 	// Write the updated inode back to the disk
-	disk->write(1 + (inumber - 1) / INODES_PER_BLOCK, inodeBlock.data);
+	disk->write(find_inode_block(inumber), inodeBlock.data);
 
 	// Return the total number of bytes written
 	return bytesWritten;
--- fs.h
+++ fs.h
@@ -3,7 +3,10 @@
 
 #include "disk.h"
 #include <vector>
-
+#include <utility>
+#include <optional>
+#include <iterator>
+#include<algorithm>
 class INE5412_FS
 {
 public:
@@ -52,25 +55,63 @@ public:
     int  fs_umount()
 ;
     int  fs_create();
-    int  find_free_inode();
     int  fs_delete(int inumber);
     int  fs_getsize(int inumber);
 
     int  fs_read(int inumber, char *data, int length, int offset);
-    int find_free_iblock();
     int  fs_write(int inumber, const char *data, int length, int offset);
 
+    
 private:
     Disk *disk;
     fs_superblock superblock;
     bool mounted = false;
     vector<bool> free_blocks;
 
+
+    bool inumber_is_valid(int inumber) {
+        return inumber > 0 && inumber <= superblock.ninodes;
+    }
+
+    int find_inode_block(int inumber) {
+        return 1 + (inumber - 1) / INODES_PER_BLOCK;
+    }
+
+    int find_inode_offset(int inumber) {
+        return (inumber - 1) % INODES_PER_BLOCK;
+    }
+
     fs_block read_block(int blocknum) {
         fs_block block;
         this->disk->read(blocknum, block.data);
         return block;
     }
+
+    fs_block read_block(fs_inode* inode, int offset, int** indirect_block_ptr) {
+        int blockIndex = offset / Disk::DISK_BLOCK_SIZE;
+        int blockNum = (blockIndex < POINTERS_PER_INODE)
+                        ? inode->direct[blockIndex]
+                        : inode->indirect;
+        
+        if (blockIndex >= POINTERS_PER_INODE) {
+            if (!indirect_block_ptr) {
+                fs_block indirect = read_block(inode->indirect);
+                *indirect_block_ptr = new int[POINTERS_PER_BLOCK];
+                for (int i = 0; i < POINTERS_PER_BLOCK; ++i)
+                  (*indirect_block_ptr)[i] = indirect.pointers[i];
+            }
+            return read_block((*indirect_block_ptr)[blockIndex - POINTERS_PER_INODE]);
+        }
+        
+        return read_block(blockNum);
+    }
+
+    optional<pair<int,fs_block>> find_free_inode();
+
+    bool is_usable();
+    bool is_usable(int inumber);
+
+    int find_free_iblock();
 };
 
 #endif
